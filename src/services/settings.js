25b29127b723ca4a213d0fe9e5e0a150f31881fe
saravanaecom <saravanaecomweb@gmail.com>
2025-03-04 15:44:53 +0530

    Add files via upload

--- src/services/settings.js
+++ src/services/settings.js
@@ -60,13 +60,44 @@ export const API_FetchSelectSettingsNew = async () => {
         throw error; // Re-throw so the calling function can handle it
     }
 };
+ 
 
+export const API_Fetchpincode = async () => { 
+    let objData = "";
+    let objlist = {
+        Comid: ServerURL.COMPANY_REF_ID,
+    };
 
+    try {
+        const response = await fetch(`${APIRoutes.GET_PINCODE}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+                objData: objData,
+            },
+            body: JSON.stringify(objlist),
+        });
 
+        if (!response.ok) {
+            throw new Error('Network response was not ok.');
+        }
 
+        const data = await response.json();
+        console.log(data);  // Check the response structure
 
+        // Check if the response is a valid array (not wrapped inside a "data" property)
+        if (!Array.isArray(data)) {
+            throw new Error('No valid array data found.');
+        }
 
+        // Return the data directly, as it is the array you need
+        return data;
 
+    } catch (error) {
+        console.error('Failed to fetch details:', error);
+        throw error;  // Re-throw so the calling function can handle the error
+    }
+};
 
 
 export const API_FetchDeliveryTimes = async () => {
